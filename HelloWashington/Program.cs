90962345fe08eeb5c19ab2043b23d850c6eb028b
kathleen2 <krkaynor2@gmail.com>
2016-10-09 09:05:45 -0700

    Returns Hello to name input

--- HelloWashington/Program.cs
+++ HelloWashington/Program.cs
@@ -7,7 +7,11 @@ namespace HelloWashington
     {
         static void Main()
         {
-            Console.WriteLine("Hello Olympia");
+            Console.WriteLine("Hello, I live in Olympia.");
+            Console.WriteLine("Where do you live?");
+            Console.WriteLine("My name is Toshiba. What's your name?");
+            string Input = Console.ReadLine();
+            Console.WriteLine("Hi " + Input + "! It's nice to meet you.");
             Console.ReadLine();
         }
     }
